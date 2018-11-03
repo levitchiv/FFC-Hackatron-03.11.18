d409680db9b2dcf70f847660b8453411d472550f
Alex Scheaua <alex.scheaua22@gmail.com>
2018-11-03 20:02:29 +0200

    database injection

--- Extra6 - googleAPI/showMarkers.js
+++ Extra6 - googleAPI/showMarkers.js
@@ -2,13 +2,16 @@
 $(document).ready(function(){
   var location = [];
   $.ajax({
-    type: 'GET',
-    url: 'mapObjects.json',
+    type: 'POST',
+    url: 'https://vadim-hasura.herokuapp.com/v1alpha1/graphql',
+    contentType: "application/json",
+    dataType: 'json',
+    data: JSON.stringify({query:"query {\n  found_objects {\n    id\n    email\n    name\n    phone\n    category\n    description\n   \titem_name\n    lat\n    lng\n  }\n}",variables:null}),
     success: function(result){
       location = result;
     }
   })
-  var infowindow = []
+  var infowindow = [];
   var marker  = [];
   //functie care arata obuectele pierdute pe harta
   $("#showMarker").on("click", function(){
@@ -23,7 +26,7 @@ $(document).ready(function(){
         content:("<h2>Object: " + location[i].id + "</h2> <h5> Category: " + location[i].category + "</h5><button type = 'button' class = 'infoButton' id = " + location[i].id + "> Contact </button>")
       });
       var infoWind = infowindow[i];
-      google.maps.event.addListener(marker, 'click', function() {
+      google.maps.event.addListener(marker[i], 'click', function() {
       infoWind.open(map,marker[i]);
       });
     };
