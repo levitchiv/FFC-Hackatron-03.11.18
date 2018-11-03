a21510c247420a279b409aa59efa939390a740fa
Alex Scheaua <alex.scheaua22@gmail.com>
2018-11-03 19:14:28 +0200

    GMapsApi load bug
    gmapsApi loads first, then other js

--- Extra6 - googleAPI/showMarkers.js
+++ Extra6 - googleAPI/showMarkers.js
@@ -8,22 +8,22 @@ $(document).ready(function(){
       location = result;
     }
   })
-
-
+  var
+  var marker  = [];
   //functie care arata obuectele pierdute pe harta
   $("#showMarker").on("click", function(){
     $("#mapContainer").slideDown("speed: slow");
     for(var i = 0; i < location.length; i++){
       //creez marker pe coordonatele din obiectul salvat in "location[i]" de pe server
-      var marker = new google.maps.Marker({position: location[i].coords});
-      marker.setMap(map);
+      marker[i] = new google.maps.Marker({position: location[i].coords});
+      marker[i].setMap(map);
       //creez info pentru marker cu buton pentru afisare content
       var infowindow = new google.maps.InfoWindow({
         content:("<h2>Object: " + location[i].id + "</h2> <h5> Category: " + location[i].category + "</h5><button type = 'button' class = 'infoButton' id = " + location[i].id + "> Contact </button>")
-        });
+      });
 
       google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map,marker);
+      infowindow.open(map,marker[i]);
       });
     };
   })
