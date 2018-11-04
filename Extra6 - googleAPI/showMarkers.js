31f5b0f30e2bddeaf2f7d112b18fd2696de3e31c
Alex Scheaua <alex.scheaua22@gmail.com>
2018-11-04 10:42:18 +0200

    Map Api bug + InfoWindows for markers

--- Extra6 - googleAPI/showMarkers.js
+++ Extra6 - googleAPI/showMarkers.js
@@ -9,32 +9,30 @@ $(document).ready(function(){
     data: JSON.stringify({query:"query {\n  found_objects {\n    id\n    email\n    name\n    phone\n    category\n    description\n   \titem_name\n    lat\n    lng\n  }\n}",variables:null}),
     success: function(result){
       location = result;
-      console.log(location.data);
       location = location.data.found_objects;
     }
   })
 
-  // var infowindow = [];
-  var marker  = [];
   //functie care arata obuectele pierdute pe harta
   $("#showMarker").on("click", function(){
     $("#mapContainer").slideDown("speed: slow");
-    for(var i = 0; i < location.length; i++){
+    var i = 0;
 
+    Array.prototype.forEach.call(location, function(){
       var coords = new google.maps.LatLng(location[i].lat, location[i].lng);
       //creez marker pe coordonatele din obiectul salvat in "location[i]" de pe server
-      marker[i] = new google.maps.Marker({position: coords} );
-      marker[i].setMap(map);
+      var marker = new google.maps.Marker({position: coords} );
+      marker.setMap(map);
       //creez info pentru marker
       var infowindow = new google.maps.InfoWindow({
-        content:("dsada")
+        content:(location[i].name)
       });
 
-      var infoWind = infowindow;
-      console.log("dsa" + infowindow);
-      // google.maps.event.addListener(marker[i], 'click', function() {
-      // infowindow.open(map,marker[i]);
-      // });
-    };
+      google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+      });
+      i++;
+    })
+
   })
 })
