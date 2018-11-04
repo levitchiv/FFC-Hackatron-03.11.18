d333ccb5aa055f3040e4233ccc69ce1b51fc04c9
Alex Scheaua <alex.scheaua22@gmail.com>
2018-11-04 17:11:09 +0200

    Update Form 2 alpha

--- showMarkers.js
+++ showMarkers.js
@@ -51,7 +51,7 @@ function loadFormspree(item){
     $("#mapContainer").slideUp("speed: slow");
     $("#wrapper").html(`'<section class="formspree">
           <h2>Lost & Found</h2>
-          <p>Did you lose something? Please give us some details about your lost item and get in touch with the person who found it.<br></p>
+          <p>Did you lose something? Please give us some details about your lost item and get in touch with the person who found it.<br><br> The person that found this item: `+ item.name +`<br>Their phone: `+ item.phone +`<br>Their email address: `+item.email +`</p>
           <form action="https://formspree.io/`+ item.email +`" method="POST">
             <input type="text" id="myName" name="Name" placeholder="Enter your name...">
             <input type="email" id="myEmail" name="Email" placeholder="Enter your email...">
